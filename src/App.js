de772641bf114a44668903a1694eb7b9392b0bda
= <adedejifavour6@gmail.com>
2022-11-01 12:57:12 +0100

    Refactor to functional component and import neccessary component used in routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => (
+  <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quotes />} />
+      </Routes>
+    </Router>
+  </>
+
+);
 
 export default App;

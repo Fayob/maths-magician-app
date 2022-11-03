5ed5269945d76c42da3de1070f05a88f174d93ec
Derrick <djmulinde@gmail.com>
2022-11-03 16:10:18 +0300

    Update Calculator test

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Calculator from '../components/Calculator';
@@ -7,5 +7,11 @@ import Calculator from '../components/Calculator';
 describe('test for Calculator Component', () => {
   it('should render Calculator Component', () => {
     expect(render(<Calculator />)).toMatchSnapshot();
+  });
+
+  it('should render Calculator Component', () => {
+    render(<Calculator />);
+    expect(screen.getByText('Let`s do some math!')).not.toBeNull();
+    expect(screen.queryByText('8')).toBeInTheDocument();
   });
 });

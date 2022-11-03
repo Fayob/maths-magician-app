29b9ccc02f5a0efdabe44714449b0ab2824c3296
= <adedejifavour6@gmail.com>
2022-11-03 12:54:13 +0100

    Use wrapper for Navbar Component testing

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Navbar from '../components/Navbar';
+
+describe('test for Navbar Component', () => {
+  it('should render Navbar Component', () => {
+    expect(render(<Navbar />, { wrapper: BrowserRouter })).toMatchSnapshot();
+  });
+
+  // it('should simulate user interaction', () => {
+  //   expect()
+  // })
+});
